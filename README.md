84d82555c360d47ccd0c9f78d6e362023fa2b4c0
noami <noamisraeli97@gmail.com>
2022-12-06 20:26:38 +0200

    Finishes

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # web3_approvers
+
+## HOWTO
+``` 
+pip install -r requirements.txt
+
+python3 my_approvals.py --address 0xe41134e.... 
+```
+
+for verbose logging:
+
+```
+python3 my_approvals.py --address 0xe41134e.... --verbose
+```
+
--- my_approvals.py
+++ my_approvals.py
@@ -7,6 +7,15 @@
 # A regular transfer is based on recording a balance book of all participants in the token's trading game, while transferFrom is additionally based on the permited amount of tokens that can be traded based on a single approve.
 # if A approves B with an amount of 1 token, the transferFrom action B can do is limitted to 1 token.
 
+import sys
+import logging
+import typing
+import json
+import sha3
+import pydantic
+
+import typer
+
 from web3 import Web3
 from ens import ENS
 
@@ -14,105 +23,142 @@ from web3.middleware import geth_poa_middleware
 from eth_abi.codec import ABICodec
 from web3._utils.events import get_event_data
 
-E20_APPROVAL_ABI = {
-            "anonymous": False,
-            "inputs": [
-                {
-                    "indexed": True,
-                    "name": "owner",
-                    "type": "address"
-                },
-                {
-                    "indexed": True,
-                    "name": "spender",
-                    "type": "address"
-                },
-                {
-                    "indexed": False,
-                    "name": "value",
-                    "type": "uint256"
-                }
-            ],
-            "name": "Approval",
-            "type": "event"
-        }
+
+_logger = logging.getLogger(__name__)
+_logger.addHandler(logging.StreamHandler(sys.stdout))
+ 
+class ABIParam(pydantic.BaseModel):
+    indexed: bool
+    name: str
+    type: str
 
 
-def get_signature_string_from_event_abi(event_abi):
-    return f"{event_abi.get('name')}{tuple(param['type'] for param in event_abi.get('inputs'))}".replace(" ", "").replace("'", "")
+class EventABI(pydantic.BaseModel):
+    inputs: typing.List[ABIParam]
+    name: str
+    type = 'event'
 
-def get_event_signature(event_abi):
-    signature_string = get_signature_string_from_event_abi(event_abi)
-    import sha3
-    k = sha3.keccak_256()
+    @property
+    def signature_string(self) -> str:
+        params_string = ",".join(param.type for param in self.inputs)
+        return f"{self.name}({params_string})"
 
-    k.update(signature_string.encode())
-    return "0x" + k.hexdigest()
+    @property
+    def signature_hex(self):
+        k = sha3.keccak_256()
+
+        k.update(self.signature_string.encode())
+        return "0x" + k.hexdigest() 
+
+E20_APPROVAL_ABI_JSON = """{
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "owner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "spender",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Approval",
+        "type": "event"
+}"""
+
+
+API_KEY = "7e135dbc63904c30808f84ff52a92343"
+
+BASE_URL = f"https://mainnet.infura.io/v3/{API_KEY}"
+
+ADDRESS_TO_NAME_MAPPING = {
+    '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45': 'Uniswap V3: Router 2',
+    '0x9fa69536d1cda4A04cFB50688294de75B505a9aE': 'DeRace: DERC Token',
+    '0x9AAb3f75489902f3a48495025729a0AF77d4b11e': 'Kyber: Deployer 3',
+    '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D': 'Uniswap V2: Router 2',
+    '0x3dfd23A6c5E8BbcFc9581d2E864a68feb6a076d3': 'Aave: Lending Pool Core V1',
+    '0x5e3Ef299fDDf15eAa0432E6e66473ace8c13D908': 'Polygon (Matic): PoS Staking Contract',
+    '0x111111125434b319222CdBf8C261674aDB56F3ae': '1inch v2: Aggregation Router',
+    '0xECf0bdB7B3F349AbfD68C3563678124c5e8aaea3': 'Kyber: Staking'
+}
+
 
 def get_address_as_topic(address: str):
     return '0x000000000000000000000000' + address[-40:]
 
-def validate_address(w3: Web3, address: str):
-    assert w3.is_address(address)
-
 def get_name_from_address(ns: ENS, address: str):
-    name = ns.name(address) or address # not working 
+    name = ns.name(address) # not working for some reason
 
     if name is None:
-        MAPPING = {
-            '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45': 'Uniswap V3: Router 2'
-        }
-        return MAPPING.get(address)
-
+        return ADDRESS_TO_NAME_MAPPING.get(address) or address
     else:
         return name
 
-if __name__ == '__main__':
-    ACCOUNT_ADDRESS = "0x005e20fCf757B55D6E27dEA9BA4f90C0B03ef852" # TODO: from args
+
+def main(
+    account_address: str = typer.Option(..., '--address'), 
+    url: typing.Optional[str] = typer.Option(None, '--eth-node-api'),
+    verbose: bool = typer.Option(False, '--verbose')
+    ):
+    base_url = url or BASE_URL
+    if verbose:
+        _logger.setLevel(logging.DEBUG)
+    else:
+        _logger.setLevel(logging.INFO)
     
-    API_KEY = "7e135dbc63904c30808f84ff52a92343"
+    abi_as_dict = json.loads(E20_APPROVAL_ABI_JSON)
 
-    url = f"https://mainnet.infura.io/v3/{API_KEY}"
-
-    provider = Web3.HTTPProvider(url)
-
+    abi = EventABI.parse_obj(abi_as_dict)
+    
+    provider = Web3.HTTPProvider(base_url)
     w3 = Web3(provider)
 
     name_resolving = ENS.fromWeb3(w3)
 
-    name_resolving.name('0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45')
-
-    # for compatibility of the POA
+    # for compatibility of the POA api
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
     filter_by_approve_event = w3.eth.filter(
-        {
-            'topics': [
-            get_event_signature(E20_APPROVAL_ABI), 
-            get_address_as_topic(ACCOUNT_ADDRESS)
+        dict(
+            topics=[
+            abi.signature_hex, 
+            get_address_as_topic(account_address)
             ], 
-            'fromBlock': 0
-    })
+            fromBlock=0
+        )
+    )
 
     codec: ABICodec = w3.codec
 
-    different_events = set()
-    different_spenders = set()
-    different_transactions = set()
+    all_events = filter_by_approve_event.get_all_entries()
 
-    for event in filter_by_approve_event.get_all_entries():
-        # Using an internal function for parsing the entries result
-        event_args = get_event_data(codec, event_abi=E20_APPROVAL_ABI, log_entry=event)['args']
+    if len(all_events) is None:
+        _logger.info("There are no approvals related to this address")
+    else:
+        for event in all_events:
+            # Using an internal function for parsing the entries result
+            event_args = get_event_data(codec, event_abi=abi_as_dict, log_entry=event)['args']
 
-        spender_name = get_name_from_address(name_resolving, event_args['spender'])
-        
-        message = f"Approval on {spender_name} on amount of {event_args['value']}"
-        print(message)
+            spender_name = get_name_from_address(name_resolving, event_args['spender'])
+            value_approved = event_args['value']
 
-        different_events.add(message)
-        different_transactions.add(event['transactionHash'])
-        different_spenders.add(spender_name)
+            transaction_id =  event['transactionHash']
+            log_index = event['logIndex']
+
+            message = f"Approval on {spender_name} on amount of {value_approved}"
+            _logger.info(message, extra=dict(tsx_id=transaction_id))
+            _logger.debug(f"transaction id: {transaction_id.hex()} " \
+                          f"log_index: {log_index}")
 
 
-    print(f"Total spenders: {different_spenders}")
-    print(f"Total transactions: {different_transactions}")
+if __name__ == '__main__':
+    typer.run(main)
+    
+    
